737ead78291f30ea0afcef0b4032eb04c1ae92c3
orchid <o.kosobreev@yandex.ru>
2025-06-12 02:37:42 +0300

    pend updt

--- resources/views/flowers/parts/blooms/peduncle.blade.php
+++ resources/views/flowers/parts/blooms/peduncle.blade.php
@@ -1,7 +1,7 @@
 <div class="row mb-3">
 	<label class="col-form-label" for="peduncle">{{ __('bloom.peduncle') }}
 	<select class="form-select" name="peduncle" id="peduncle">
-		<option value="1" {{ old('peduncle', $Peduncle ?? null) == 1 ? 'selected' : '' }}>{{ __('basic.new') }}</option>
-		<option value="0" {{ old('peduncle', $Peduncle ?? null) == 0 ? 'selected' : '' }}>{{ __('basic.old') }}</option>
+		<option value="0" {{ old('peduncle', $Peduncle ?? 1) == 0 ? 'selected' : '' }}>{{ __('basic.old') }}</option>
+		<option value="1" {{ old('peduncle', $Peduncle ?? 1) == 1 ? 'selected' : '' }}>{{ __('basic.new') }}</option>
 	</select></label>
 </div>
