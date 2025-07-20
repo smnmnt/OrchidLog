b0c6b87409cf653a64ac4ac9edb7376a27e3b644
smnmnt <o.kosobreev@yandex.ru>
2025-07-20 15:01:27 +0300

    wtring fix

--- app/Http/Controllers/FlowersController.php
+++ app/Http/Controllers/FlowersController.php
@@ -85,22 +85,22 @@ class FlowersController extends Controller
     public function watering_index()
     {
         $waterings = DB::table('flower_waterings')
-            ->leftJoin('watering_types_of', 'flower_waterings.TypeID', '=', 'watering_types_of.ID')
-            ->leftJoin('fertilizers', 'flower_waterings.FertilizerID', '=', 'fertilizers.ID')
-            ->leftJoin('watering_groups', 'flower_waterings.GroupID', '=', 'watering_groups.ID')
-            ->leftJoin('flower_watering_links', 'flower_waterings.ID', '=', 'flower_watering_links.WateringID')
-            ->select(
-                'flower_waterings.*',
-                'watering_types_of.WateringName',
-                'watering_types_of.TypeOfImg',
-                'fertilizers.Name as FertilizerName',
-                'watering_groups.Name as GroupName',
-                DB::raw('COUNT(flower_watering_links.FlowerID) as FlowerCount')
-            )
-            ->groupBy('flower_waterings.ID')
-            ->orderByDesc('WateringDate')
-            ->orderByDesc('updated_at')
-            ->get();
+			->select(
+				'flower_waterings.*',
+				'watering_types_of.WateringName',
+				'watering_types_of.TypeOfImg',
+				'fertilizers.Name as FertilizerName',
+				'watering_groups.Name as GroupName',
+				DB::raw('COUNT(flower_watering_links.FlowerID) as FlowerCount')
+			)
+			->leftJoin('watering_types_of', 'flower_waterings.TypeID', '=', 'watering_types_of.ID')
+			->leftJoin('fertilizers', 'flower_waterings.FertilizerID', '=', 'fertilizers.ID')
+			->leftJoin('watering_groups', 'flower_waterings.GroupID', '=', 'watering_groups.ID')
+			->leftJoin('flower_watering_links', 'flower_waterings.ID', '=', 'flower_watering_links.WateringID')
+			->groupBy('flower_waterings.ID') // + все поля из SELECT если режим включён
+			->orderByDesc('WateringDate')
+			->orderByDesc('updated_at')
+			->get();
 
         return view('watering_index', compact('waterings'));
     }
